ad29f747d6b4c7abfbdda41c30171066af325beb
dexterski007 <mbouj007@gmail.com>
2024-08-19 11:33:50 +0100

    task1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Basic caching system
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Fifo cache """
+    def __init__(self):
+        """ constructor func """
+        super().__init__()
+
+    def put(self, key, item):
+        """ put function """
+        if key or item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded = next(iter(self.cache_data))
+            self.cache_data.pop(discarded)
+            print("DISCARD: {}".format(discarded))
+
+    def get(self, key):
+        """ get item"""
+        return self.cache_data.get(key, None)
