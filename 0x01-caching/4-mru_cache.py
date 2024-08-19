b8446895ccb967788030fa51a44e42d94695043e
dexterski007 <mbouj007@gmail.com>
2024-08-19 16:24:19 +0100

    task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -4,21 +4,28 @@
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """ Lifo cache """
+class MRUCache(BaseCaching):
+    """ Mru cache """
     def __init__(self):
         """ constructor func """
         super().__init__()
 
     def put(self, key, item):
         """ put function """
+        if key is None or item is None:
+            return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             discarded = next(reversed(self.cache_data))
             self.cache_data.pop(discarded)
             print("DISCARD: {}".format(discarded))
-        if key or item:
-            self.cache_data[key] = item
+        if key in self.cache_data.keys():
+            self.cache_data.pop(key)
+        self.cache_data[key] = item
 
     def get(self, key):
         """ get item"""
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data.keys():
+            return None
+        item = self.cache_data.pop(key)
+        self.cache_data[key] = item
+        return item
