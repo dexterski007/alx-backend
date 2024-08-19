3d28bd024b6eaf5c241349ec56d3739fc74a494e
dexterski007 <mbouj007@gmail.com>
2024-08-19 11:09:38 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Basic caching system
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ basic cache """
+
+    def put(self, key, item):
+        """ put new items in cache """
+        if key or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ get item value from cache """
+        return self.cache_data.get(key, None)
