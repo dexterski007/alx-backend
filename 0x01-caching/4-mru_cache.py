4999599a18fb9940a5d6ab4a394705d15dda2554
dexterski007 <mbouj007@gmail.com>
2024-08-20 21:22:21 +0100

    fix

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -5,27 +5,25 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ Mru cache """
+    """ Mru cache fix """
     def __init__(self):
         """ constructor func """
         super().__init__()
 
     def put(self, key, item):
-        """ put function """
-        if key is None or item is None:
-            return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded = next(reversed(self.cache_data))
-            self.cache_data.pop(discarded)
-            print("DISCARD: {}".format(discarded))
-        if key in self.cache_data.keys():
-            self.cache_data.pop(key)
-        self.cache_data[key] = item
+        """ put function fix """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded = next(reversed(self.cache_data))
+                self.cache_data.pop(discarded)
+                print("DISCARD: {}".format(discarded))
+            if key in self.cache_data.keys():
+                self.cache_data.pop(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ get item"""
-        if key is None or key not in self.cache_data.keys():
-            return None
-        item = self.cache_data.pop(key)
-        self.cache_data[key] = item
-        return item
+        """ get item fixed """
+        if key and key in self.cache_data.keys():
+            item = self.cache_data.pop(key)
+            self.cache_data[key] = item
+            return item
