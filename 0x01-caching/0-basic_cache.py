377b09bfcc418fec8f8540b34ae9a57e767ad730
dexterski007 <mbouj007@gmail.com>
2024-08-20 13:13:26 +0100

    and

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,9 +9,9 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """ put new items in cache """
-        if key or item:
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
         """ get item value from cache """
-        return self.cache_data.get(key, None)
+        return self.cache_data.get(key)
