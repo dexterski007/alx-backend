faeaad0e6c18393f8f867c954c7295efb96edcf8
dexterski007 <mbouj007@gmail.com>
2024-08-20 21:05:16 +0100

    fix

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,14 +11,14 @@ class LIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
-        """ put function """
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded = next(reversed(self.cache_data))
-            self.cache_data.pop(discarded)
-            print("DISCARD: {}".format(discarded))
+        """ put function func """
         if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded = next(reversed(self.cache_data))
+                self.cache_data.pop(discarded)
+                print("DISCARD: {}".format(discarded))
             self.cache_data[key] = item
 
     def get(self, key):
-        """ get item"""
+        """ get item doc"""
         return self.cache_data.get(key, None)
