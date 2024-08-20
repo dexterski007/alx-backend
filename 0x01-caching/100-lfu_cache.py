cedfd2dc946ba0dd9620f95e4d8c5bdc3430ad09
dexterski007 <mbouj007@gmail.com>
2024-08-20 21:34:04 +0100

    fix

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -7,30 +7,28 @@ from base_caching import BaseCaching
 class LFUCache(BaseCaching):
     """ Lfu cache """
     def __init__(self):
-        """ constructor func """
+        """ constructor func fix """
         super().__init__()
         self.freq = {}
         self.usage = {}
 
     def put(self, key, item):
-        """ put function """
-        if key is None or item is None:
-            return
-        if key in self.cache_data.keys():
-            self.cache_data.pop(key)
-        self.cache_data[key] = item
-        if key in self.freq.keys():
-            self.freq[key] += 1    
-        else:
-            self.freq[key] = 0
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded = next(iter(self.cache_data))
-            self.cache_data.pop(discarded)
-            print("DISCARD: {}".format(discarded))
+        """ put function fix """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data.pop(key)
+            self.cache_data[key] = item
+            if key in self.freq.keys():
+                self.freq[key] += 1    
+            else:
+                self.freq[key] = 0
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discarded = next(iter(self.cache_data))
+                self.cache_data.pop(discarded)
+                print("DISCARD: {}".format(discarded))
 
     def get(self, key):
-        """ get item"""
-        if key is None or key not in self.cache_data.keys():
-            return None
-        self.freq[key] += 1
-        return self.cache_data[key]
+        """ get item fix """
+        if key and key in self.cache_data:
+            self.freq[key] += 1
+            return self.cache_data[key]
