ae53fb79240bd827ae8bbecb5a05fdab94e0826d
dexterski007 <mbouj007@gmail.com>
2024-08-27 14:23:04 +0100

    task3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,7 +27,7 @@ def get_locale() -> str | None:
 @app.route("/")
 def index() -> str:
     """Get index route for flask app."""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
