abe928f7ed0504f1d0f04468fc6d8fb8ab770166
dexterski007 <mbouj007@gmail.com>
2024-08-27 15:05:18 +0100

    task4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Flask app documentation."""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Config class for babel."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale function for babel."""
+    locale = request.args.get('locale')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index() -> str:
+    """Get index route for flask app."""
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
